5801fcad32774d98616f507bd8b4cb6a2c36836b
Mina Matta <minamatta98@gmail.com>
2024-08-15 09:27:49 +1000

    Added lualine lazy nvim config

---
+++ lua/plugins/ui/line.lua
@@ -0,0 +1,68 @@
+local colors = {
+  bg       = '#202328',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+}
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed
+    }
+  end
+end
+
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+  	options = {
+  	  -- theme = bubbles_theme,
+  	  component_separators = { left = '', right = '' },
+  	  section_separators = { left = '', right = '' },
+  	},
+  	sections = {
+  	  lualine_a = { { 'mode', separator = { left = '' }, right_padding = 5 } },
+  	  lualine_b = { 'filename', {'b:gitsigns_head', icon = ''}, {'diff', source = diff_source},
+	  { 'diagnostics',
+  		sources = { 'nvim_diagnostic' },
+  		symbols = { error = ' ', warn = ' ', info = ' ' },
+  		diagnostics_color = {
+    		 error = { fg = colors.red },
+    		 warn = { fg = colors.yellow },
+    		 info = { fg = colors.cyan },
+	},
+         }, 
+  	 },
+  	  lualine_c = {
+  	    '%=', --[[ add your center compoentnts here in place of this comment ]]
+  	  },
+  	  lualine_x = {'encoding'},
+  	  lualine_y = { 'fileformat','filetype', 'progress' },
+  	  lualine_z = {
+  	    { 'location', separator = { right = '' }, left_padding = 3 },
+  	  },
+  	},
+  	inactive_sections = {
+  	  lualine_a = { 'filename' },
+  	  lualine_b = {},
+  	  lualine_c = {},
+    	  lualine_x = {},
+  	  lualine_y = { },
+  	  lualine_z = { 'location' },
+  	},
+  	tabline = {},
+  	extensions = {},
+    }
+}
