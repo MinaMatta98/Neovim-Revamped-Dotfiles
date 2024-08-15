bec53b88294d107b4ec1c6c381458eab96ccc455
Mina Matta <minamatta98@gmail.com>
2024-08-15 23:28:55 +1000

    Changed the definition for VimRC edit keymap

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- This is the only setting that must be set before loading lazy
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>V", ":edit $MYVIMRC<CR>", { silent = true, desc = "Edit Vim Rc" })
+vim.keymap.set("n", "<leader>V", ":edit $MYVIMRC<CR>", { silent = true, desc = "Vimrc file" })
 require("config.lazy")
 require("config.setup")
