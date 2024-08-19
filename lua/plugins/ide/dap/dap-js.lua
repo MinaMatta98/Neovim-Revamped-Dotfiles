8c229be2cc513f1acbd4f984774f88dac34452b1
Mina Matta <minamatta98@gmail.com>
2024-08-20 09:09:21 +1000

    Commented out mason-registry. Note that an investigation is needed as to why mason-registry does not work despite mason being required as a dep

--- lua/plugins/ide/dap/dap-js.lua
+++ lua/plugins/ide/dap/dap-js.lua
@@ -6,11 +6,12 @@ return {
 			"microsoft/vscode-js-debug",
 			build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
 		},
-		"jay-babu/mason-nvim-dap.nvim",
-		"williamboman/mason.nvim",
+		{ "mason-nvim-dap", dependencies = { "mason" } },
 	},
 	opts = {
 		-- debugger_path = require("mason-registry").get_package("js"):get_install_path() .. "/js-debug-adapter",
+		-- debugger_path = require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+			-- .. "/vscode-js-debug",
 		debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
 		adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
 		-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
