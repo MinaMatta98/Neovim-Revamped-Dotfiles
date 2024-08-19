2d4037c461c67b7b82609c04568e5447a812c303
Mina Matta <minamatta98@gmail.com>
2024-08-20 09:07:10 +1000

    This is a plugin that helps track the current closure conditions for closures that are more than 100vh in height, such that the definition does not need to be manually parsed

---
+++ lua/plugins/ide/lsp/biscuits.lua
@@ -0,0 +1,24 @@
+return {
+	"code-biscuits/nvim-biscuits",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	enabled = true,
+	opts = {
+		default_config = {
+			-- max_length = 12,
+			-- min_distance = 5,
+			prefix_string = " ✨ ",
+		},
+		-- language_config = {
+		-- 	html = {
+		-- 		prefix_string = " 🌐 ",
+		-- 	},
+		-- 	javascript = {
+		-- 		prefix_string = " ✨ ",
+		-- 		max_length = 80,
+		-- 	},
+		-- 	python = {
+		-- 		disabled = true,
+		-- 	},
+		-- },
+	},
+}
