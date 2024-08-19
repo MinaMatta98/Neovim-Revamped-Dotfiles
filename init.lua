b4d17087e8f8047b328d1692d20bd858ce947b3c
Mina Matta <minamatta98@gmail.com>
2024-08-20 09:25:09 +1000

    Added vim.o.number for relative number settings nvim

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- This is the only setting that must be set before loading lazy
 vim.g.mapleader = " "
+vim.o.number = true
 vim.keymap.set("n", "<leader>V", ":edit $MYVIMRC<CR>", { silent = true, desc = "Vimrc file" })
 require("config.lazy")
 require("config.setup")
